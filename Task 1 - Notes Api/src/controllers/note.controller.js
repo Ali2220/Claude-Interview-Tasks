14d20408b17cc6134ae7798917b4e82d12c64973
Ali2220 <alisarwar0277@gmail.com>
2026-06-23 20:58:00 +0500

    task 1: update note api, and delete note api created

--- Task 1 - Notes Api/src/controllers/note.controller.js
+++ Task 1 - Notes Api/src/controllers/note.controller.js
@@ -17,7 +17,9 @@ const createNote = async (req, res, next) => {
 
         res.status(201).json({ message: 'Note create successfully', note })
     } catch (error) {
-
+        res.status(500).json({
+            error: `Create Note Error: ${err.message}`
+        })
     }
 }
 
@@ -46,13 +48,82 @@ const getNoteById = async (req, res, next) => {
         }
 
         if (note.owner.toString() !== req.user._id.toString()) {
-            return res.status(403).json({ message: "You are not authorized to access this note"})
+            return res.status(403).json({ message: "You are not authorized to access this note" })
         }
 
-        return res.status(400).json({ note })
+        return res.status(200).json({ note })
     } catch (err) {
-
+        res.status(500).json({
+            error: `Get note by id error: ${err}`
+        })
     }
 }
 
-module.exports = { createNote, getNotes, getNoteById }
+const updateNote = async (req, res, next) => {
+    try {
+        const note = await Note.findById(req.params.id)
+
+        if (!note) {
+            return res.status(404).json({
+                message: "Note not found"
+            })
+        }
+
+        if (note.owner.toString() !== req.user._id.toString()) {
+            return res.status(403).json({
+                message: 'You are not authorized to update this note'
+            })
+        }
+
+        const { title, content } = req.body
+
+        if (title) {
+            note.title = title
+        }
+
+        if (content) {
+            note.content = content
+        }
+
+        await note.save()
+
+        res.status(200).json({ message: "Note updated successfully", note })
+    } catch (err) {
+        res.status(500).json({
+            error: `Update note error ${err}`
+        })
+    }
+}
+
+// Delete Note
+const deleteNote = async (req, res, next) => {
+    try {
+
+        const note = await Note.findById(req.params.id)
+
+        if (!note) {
+            return res.status(404).json({
+                message: "Note not found"
+            })
+        }
+
+        if (note.owner.toString() !== req.user._id.toString()) {
+            return res.status(403).json({
+                message: "You are not authorized to delete this post"
+            })
+        }
+
+        await note.deleteOne()
+
+        res.status(200).json({
+            message: 'Note deleted successfully'
+        })
+
+    } catch (err) {
+        res.status(500).json({
+            error: `Delete note error ${err}`
+        })
+    }
+}
+
+module.exports = { createNote, getNotes, getNoteById, updateNote, deleteNote }
--- Task 1 - Notes Api/src/routes/note.route.js
+++ Task 1 - Notes Api/src/routes/note.route.js
@@ -1,10 +1,12 @@
 const express = require('express')
 const authMiddleware = require('../middlewares/auth.middleware')
-const { createNote, getNotes, getNoteById } = require('../controllers/note.controller')
+const { createNote, getNotes, getNoteById, updateNote, deleteNote } = require('../controllers/note.controller')
 const router = express.Router()
 
 router.post('/', authMiddleware, createNote)
 router.get('/', authMiddleware, getNotes)
 router.get('/:id', authMiddleware, getNoteById)
+router.patch('/:id', authMiddleware, updateNote)
+router.delete('/:id', authMiddleware, deleteNote)
 
 module.exports = router
